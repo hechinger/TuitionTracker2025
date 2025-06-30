b7aa85ae55f88d909c337380d55c3671bc4e2eaa
Andrew Milligan <andrew.i.milligan@gmail.com>
2025-06-30 15:11:58 -0400

    Disable one more api route for now

--- src/app/api/schools/route.ts
+++ src/app/api/schools/route.ts
@@ -1,5 +1,6 @@
-import data from "@/data/schools_index.json";
+// import data from "@/data/schools_index.json";
 
 export async function GET() {
-  return Response.json(data);
+  // return Response.json(data);
+  return Response.json({ message: "hello" });
 }
