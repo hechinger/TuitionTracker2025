bbb48bfaa21a1d09d0cac4cfaacca60011dd87e7
Andrew Milligan <andrew.i.milligan@gmail.com>
2025-07-24 06:39:46 -0400

    Tweak some pipeline code

--- src/pipeline/parsing/synthesize.ts
+++ src/pipeline/parsing/synthesize.ts
@@ -50,6 +50,7 @@ export const synthesize = (
       const netYear = netPriceYears[i - 1];
 
       const stickerPrice = stickerYear.prices;
+      const sticker = stickerPrice.price;
       const getNetPrice = (bracket: keyof typeof minMaxDiscounts) => {
         if (!netYear) {
           return {
@@ -63,7 +64,6 @@ export const synthesize = (
         }
 
         const netPrice = netYear.prices[bracket];
-        const sticker = stickerPrice.price;
         const discount = netPrice / sticker!;
         const { min, max } = minMaxDiscounts[bracket];
 
@@ -90,7 +90,7 @@ export const synthesize = (
 
       const avgPrice = getNetPrice("average");
 
-      if (stickerPrice && avgPrice.price) {
+      if (stickerPrice && (avgPrice.price || i === 0)) {
         startYears.add(yearNum);
       }
 
@@ -108,6 +108,51 @@ export const synthesize = (
         },
       };
     });
+
+    const needYears = new Set([...Array(7)].map((_, i) => baseYear - i));
+    const hasEnoughData = startYears.intersection(needYears).size >= needYears.size;
+
+      const nullMult = (
+        a: number | null = null,
+        b: number | null = null,
+      ) => {
+        if (!a && a !== 0) return null;
+        if (!b && b !== 0) return null;
+        return a * b;
+      };
+
+    years[0].netPricesByBracket = {
+      average: {
+        price: nullMult(years[1].netPricesByBracket.average.discount, years[0].stickerPrice.price),
+        min: minMaxDiscounts.average.min,
+        max: minMaxDiscounts.average.max,
+      },
+      "0_30000": {
+        price: nullMult(years[1].netPricesByBracket["0_30000"].discount, years[0].stickerPrice.price),
+        min: minMaxDiscounts["0_30000"].min,
+        max: minMaxDiscounts["0_30000"].max,
+      },
+      "30001_48000": {
+        price: nullMult(years[1].netPricesByBracket["30001_48000"].discount, years[0].stickerPrice.price),
+        min: minMaxDiscounts["30001_48000"].min,
+        max: minMaxDiscounts["30001_48000"].max,
+      },
+      "48001_75000": {
+        price: nullMult(years[1].netPricesByBracket["48001_75000"].discount, years[0].stickerPrice.price),
+        min: minMaxDiscounts["48001_75000"].min,
+        max: minMaxDiscounts["48001_75000"].max,
+      },
+      "75001_110000": {
+        price: nullMult(years[1].netPricesByBracket["75001_110000"].discount, years[0].stickerPrice.price),
+        min: minMaxDiscounts["75001_110000"].min,
+        max: minMaxDiscounts["75001_110000"].max,
+      },
+      "110001": {
+        price: nullMult(years[1].netPricesByBracket["110001"].discount, years[0].stickerPrice.price),
+        min: minMaxDiscounts["110001"].min,
+        max: minMaxDiscounts["110001"].max,
+      },
+    };
 
     if (
       years.length === 11
@@ -129,44 +174,7 @@ export const synthesize = (
       const grow = (
         n: number | null = null,
         g: number | null = growth,
-      ) => {
-        if (!g && g !== 0) return null;
-        if (!n && n !== 0) return null;
-        return g * n;
-      };
-
-      years[0].netPricesByBracket = {
-        average: {
-          price: grow(years[1].netPricesByBracket.average.discount, years[0].stickerPrice.price),
-          min: minMaxDiscounts.average.min,
-          max: minMaxDiscounts.average.max,
-        },
-        "0_30000": {
-          price: grow(years[1].netPricesByBracket["0_30000"].discount, years[0].stickerPrice.price),
-          min: minMaxDiscounts["0_30000"].min,
-          max: minMaxDiscounts["0_30000"].max,
-        },
-        "30001_48000": {
-          price: grow(years[1].netPricesByBracket["30001_48000"].discount, years[0].stickerPrice.price),
-          min: minMaxDiscounts["30001_48000"].min,
-          max: minMaxDiscounts["30001_48000"].max,
-        },
-        "48001_75000": {
-          price: grow(years[1].netPricesByBracket["48001_75000"].discount, years[0].stickerPrice.price),
-          min: minMaxDiscounts["48001_75000"].min,
-          max: minMaxDiscounts["48001_75000"].max,
-        },
-        "75001_110000": {
-          price: grow(years[1].netPricesByBracket["75001_110000"].discount, years[0].stickerPrice.price),
-          min: minMaxDiscounts["75001_110000"].min,
-          max: minMaxDiscounts["75001_110000"].max,
-        },
-        "110001": {
-          price: grow(years[1].netPricesByBracket["110001"].discount, years[0].stickerPrice.price),
-          min: minMaxDiscounts["110001"].min,
-          max: minMaxDiscounts["110001"].max,
-        },
-      };
+      ) => nullMult(n, g);
 
       [...Array(2)].forEach((_, i) => {
         const newYear = baseYear + 1 + i;
@@ -213,9 +221,6 @@ export const synthesize = (
         });
       });
     }
-
-    const needYears = new Set([...Array(7)].map((_, i) => baseYear - i - 1));
-    const hasEnoughData = startYears.intersection(needYears).size >= needYears.size;
 
     return {
       ...restSchool,
